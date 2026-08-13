3a7a26d96f70d513e867a445594ef3ebc82707be
yunko_yunko <yunko20090802@gmail.com>
2026-08-13 18:39:07 +0900

    fix websockerconfig compile error

--- src/main/java/com/dev/spiderman/config/WebSocketConfig.java
+++ src/main/java/com/dev/spiderman/config/WebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerStompEedpoints(StompEndpointRegistry registry) {
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*");
     }
